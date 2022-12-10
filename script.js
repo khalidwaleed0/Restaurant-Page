44b4af809ae44c426c325afb911ad1931c5e4b27
Khalid Waleed <khalidwaleed0@outlook.com>
2022-12-10 21:22:25 +0200

    Fixed duplicated script.js files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { appendHomeContent } from './home.js';
-import { appendMenuContent } from './menu.js';
-import { appendContactContent } from "./contact.js";
+import { appendHomeContent } from './scripts/home.js';
+import { appendMenuContent } from './scripts/menu.js';
+import { appendContactContent } from "./scripts/contact.js";
 document.getElementById("home").onclick = appendHomeContent;
 document.getElementById("menu").onclick = appendMenuContent;
 document.getElementById("contact").onclick = appendContactContent;
