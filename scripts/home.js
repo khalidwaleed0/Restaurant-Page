58504dc6a89722330cea618c3766e52f50c16fd5
Khalid Waleed <khalidwaleed0@outlook.com>
2022-12-10 21:08:40 +0200

    Small Improvements

--- scripts/home.js
+++ scripts/home.js
@@ -3,7 +3,7 @@ const homeContent = `<p>Best Ramen in Japan</p>
                         <br />
                         <p>This is where the 7th Hokagi used to eat.</p>
                         <img id="owner-image" src="images/ichiraku.png" alt="Ichiraku Ramen Owner" />
-                        <p>Order online or visit us!</p>`;
+                        <p style="margin-top: auto;">Order online or visit us!</p>`;
 const btnHome = document.getElementById("home");
 const btnMenu = document.getElementById("menu");
 const btnContact = document.getElementById("contact");
